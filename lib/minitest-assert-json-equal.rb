d61f5444e76da0c7276db814d62f362bf76cbe1d
Nat Welch <nat@natwelch.com>
2016-09-07 22:08:33 +0000

    Add tests and update readme

--- lib/minitest-assert-json-equal.rb
+++ lib/minitest-assert-json-equal.rb
@@ -1,4 +1,6 @@
 require "minitest-assert-json-equal/version"
+require "minitest"
+require "json"
 
 module MinitestAssertJsonEqual
 end
@@ -8,6 +10,31 @@ module Minitest::Assertions
     a = JSON.parse(json_a)
     b = JSON.parse(json_b)
 
+    msg = message(msg, "") {
+      "Expected #{mu_pp(a)} to match #{mu_pp(b)}."
+    }
+
     assert_equal a, b, msg
+  rescue JSON::ParserError
+    msg = message(msg, "") {
+      "JSON does not parse."
+    }
+    flunk msg
+  end
+
+  def refute_json_equal(json_a, json_b, msg=nil)
+    a = JSON.parse(json_a)
+    b = JSON.parse(json_b)
+
+    msg = message(msg, "") {
+      "Expected #{mu_pp(a)} not to match #{mu_pp(b)}."
+    }
+
+    refute_equal a, b, msg
+  rescue JSON::ParserError
+    msg = message(msg, "") {
+      "JSON does not parse."
+    }
+    flunk msg
   end
 end
